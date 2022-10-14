daede0e121f41d20d08c4fd6e0df13cd11209d58
Will Joseph <programergeek@gmail.com>
2022-10-14 08:02:34 +0000

    create navigation bar componeent for desktop and tablets

--- landing-page/components/NavigationBar.tsx
+++ landing-page/components/NavigationBar.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Children } from 'react'
 
 /**
  * A bottom navigation bar meant to be used with smaller screens.
  */
 export const BottomNavigationBar: React.FC = () => {
     return (
-    <div className= "bg-background border-2 rounded-[5px] border-black w-full h-16 grid grid-cols-4 py-1.5 fixed bottom-0" >
+    <div className= "md:hidden bg-background border-2 rounded-[5px] border-black w-full h-16 grid grid-cols-4 py-1.5 fixed bottom-0" >
         {
             // map the menu options to navigation buttons.
             // TODO: replace buttons names with objects that contain the name of the button along with its icon.
@@ -20,5 +20,45 @@ export const BottomNavigationBar: React.FC = () => {
             ))
         }
     </div>
+    )
+}
+
+type Props = {
+    children: React.ReactNode
+}
+
+/** Wrapper component for desktop and mobile navigation bars */
+const NavigationBarWrapper: React.FC<Props> = ({ ...props }) => {
+
+    const children = Children.map(props.children, (value) => value) as React.ReactNode[]
+
+    return (
+    <nav className='hidden md:grid grid-cols-[16rem_auto_28rem]'>
+        <div>
+            <a href="/">
+            {children[0]}
+            </a>
+        </div>
+        <div></div>
+        <div>
+            {children[1]}
+        </div>
+    </nav>
+    )
+}
+
+
+export const NavigationBar: React.FC = () => {
+    return (
+    <NavigationBarWrapper> 
+        <p className='font-sourceCodePro text-4xl font-medium italic tracking-widest bg-folderYellow rounded-tr-[5px] text-white text-center py-2'>Joseph</p>
+        <div className='flex justify-end flex-row pr-7'>
+            {
+                ["About Me", "Projects", "Skills", "Contact Me"].map((value) => {
+                    return <button className='font-sourceCodePro px-4 py-3 font-semibold'>{value}</button>
+                })
+            }
+        </div> 
+    </NavigationBarWrapper>
     )
 }
--- landing-page/pages/index.tsx
+++ landing-page/pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { BottomNavigationBar } from '../components'
+import { BottomNavigationBar, NavigationBar } from '../components'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <NavigationBar />
       <BottomNavigationBar />
     </div>
   )
