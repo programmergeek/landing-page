e2179a5433c852fed81ac8280d7834ddd21fd0f0
Will Joseph <programergeek@gmail.com>
2022-10-18 10:30:11 +0000

    allow primary, secondary and teriary butons to have icons in the front

--- landing-page/components/Buttons.tsx
+++ landing-page/components/Buttons.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 type ButtonProps = {
     className: string,
-    children?: React.ReactNode
+    children?: React.ReactNode,
+    icon?: React.ReactNode
 }
 
 type iconProps = {
@@ -18,19 +19,26 @@ type Props = {
 /** General button component. */
 // accepts a prop called classname to extend the styles of the component.
 const Button: React.FC<ButtonProps> = ({...props}) => {
-    return <button className={`${props.className} rounded-full px-4 py-2 italic`}> {props.children} </button>
+    return <button className={`${props.className} rounded-full px-4 py-2 italic flex`}> 
+    <span className={`${props.icon ? ' block mr-3': 'hidden'}`}>
+        {props.icon} 
+    </span>
+    <span>
+        {props.children} 
+    </span>
+        </button>
 }
 
 export const PrimaryButton: React.FC<Props> = ({...props}) => {
-    return <Button className='font-roboto font-semibold border bg-black text-white'> {props.children} </Button>
+    return <Button icon={props.icon} className='font-roboto font-semibold border bg-black text-white'> {props.children} </Button>
 }
 
 export const SecondaryButton:React.FC<Props> = ({...props}) => {
-    return <Button className='border-2 border-black font-sourceCodePro bg-transparent font-semibold'>{props.children}</Button>
+    return <Button icon={props.icon} className='border-2 border-black font-sourceCodePro bg-transparent font-semibold'>{props.children}</Button>
 }
 
 export const TertiaryButton:React.FC<Props> = ({...props}) => {
-    return <Button className='font-sourceCodePro not-italic font-semibold'>{props.children}</Button>
+    return <Button icon={props.icon} className='font-sourceCodePro not-italic font-semibold'>{props.children}</Button>
 }
 
 export const IconButton: React.FC<iconProps> = ({...props}) => {
