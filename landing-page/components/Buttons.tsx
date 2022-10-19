b438caf89918d51eb88ed39a400a1014d883f992
Will Joseph <programergeek@gmail.com>
2022-10-19 19:52:54 +0000

    allow buttons to open links in new tab

--- landing-page/components/Buttons.tsx
+++ landing-page/components/Buttons.tsx
@@ -5,19 +5,22 @@ type ButtonProps = {
     children?: React.ReactNode,
     icon?: React.ReactNode,
     href?: string
+    blank?: boolean
 }
 
 type iconProps = {
     icon: React.ReactNode,
     style: "solid" | "outline",
     label?: string,
-    href?: string
+    href?: string,
+    blank?: boolean
 }
 
 type Props = {
     children?: React.ReactNode,
     icon?: React.ReactNode,
     href?: string
+    blank?: boolean
 }
 
 /** General button component. */
@@ -26,7 +29,7 @@ const Button: React.FC<ButtonProps> = ({...props}) => {
     return (
         <button className={`${props.className} rounded-full px-4 py-2 italic`}> 
             {props.href ? (
-                <a href={props.href} className="flex">
+                <a href={props.href} target={props.blank ? "_blank": undefined} className="flex">
                     <span className={`${props.icon ? ' block mr-3': 'hidden'}`}>
                         {props.icon} 
                     </span>
@@ -50,21 +53,21 @@ const Button: React.FC<ButtonProps> = ({...props}) => {
 }
 
 export const PrimaryButton: React.FC<Props> = ({...props}) => {
-    return <Button icon={props.icon} href={props.href} className='font-roboto font-semibold border bg-black text-white'> {props.children} </Button>
+    return <Button icon={props.icon} href={props.href} blank={props.blank} className='font-roboto font-semibold border bg-black text-white'> {props.children} </Button>
 }
 
 export const SecondaryButton:React.FC<Props> = ({...props}) => {
-    return <Button icon={props.icon} href={props.href} className='border-2 border-black font-sourceCodePro bg-transparent font-semibold'>{props.children}</Button>
+    return <Button icon={props.icon} href={props.href} blank={props.blank} className='border-2 border-black font-sourceCodePro bg-transparent font-semibold'>{props.children}</Button>
 }
 
 export const TertiaryButton:React.FC<Props> = ({...props}) => {
-    return <Button icon={props.icon} href={props.href} className='font-sourceCodePro not-italic font-semibold'>{props.children}</Button>
+    return <Button icon={props.icon} href={props.href} blank={props.blank} className='font-sourceCodePro not-italic font-semibold'>{props.children}</Button>
 }
 
 export const IconButton: React.FC<iconProps> = ({...props}) => {
     return props.style === "solid" ? 
     (
-        <a href={props.href}>
+        <a href={props.href} target={props.blank ? "_blank": undefined}>
             <div className="flex flex-col w-fit">
                 <div className="flex justify-center">
                     <button className='rounded-full p-3 bg-black w-fit'>{props.icon}</button>
@@ -74,7 +77,7 @@ export const IconButton: React.FC<iconProps> = ({...props}) => {
         </a>
     ) : 
     (
-        <a href={props.href}>
+        <a href={props.href} target={props.blank ? "_blank": undefined}>
         <div className="flex flex-col w-fit">
             <div className="flex justify-cen">
                 <button className="rounded-full p-3 border-2 border-black bg-transparent">{props.icon}</button>
