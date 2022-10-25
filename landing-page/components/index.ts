98c68944502fba35ab70703e3d48cafae09c2d55
Will Joseph <programergeek@gmail.com>
2022-10-25 05:04:12 +0000

    create hero banner

--- landing-page/components/index.ts
+++ landing-page/components/index.ts
@@ -1,3 +1,4 @@
 export * from './NavigationBar'
 export * from './Buttons'
 export * from './Cards'
+export * from './HeroBanner'
--- landing-page/pages/index.tsx
+++ landing-page/pages/index.tsx
@@ -1,25 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Card, ProjectCard } from '../components'
+import { HeroBanner, NavigationBar } from '../components'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className="">
-      <ProjectCard
-      projectName='The Blog'
-      description='A personal blog built in pure react.'
-      repoLink='https://github.com/programmergeek/the-blog'
-      demoLink='https://blog.willjoseph.xyz'
-      />
-      <ProjectCard
-      projectName='The Blog'
-      description='A personal blog built in pure react.'
-      repoLink='https://github.com/programmergeek/the-blog'
-      demoLink='https://blog.willjoseph.xyz'
-      reverse
-      />
+      <NavigationBar />
+      <HeroBanner />
     </div>
   )
 }
