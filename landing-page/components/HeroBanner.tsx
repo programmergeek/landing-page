29ab23fe6789f3676711ca55f771b35b41ddf346
Will Joseph <programergeek@gmail.com>
2022-10-25 05:11:58 +0000

    remove margin around banner and increase font size of 'hello'

--- landing-page/components/HeroBanner.tsx
+++ landing-page/components/HeroBanner.tsx
@@ -3,8 +3,8 @@ import { SecondaryButton } from './Buttons'
 
 export const HeroBanner: React.FC = () => {
     return (
-        <div className='bg-folderYellow p-3 rounded-r-md mr-2'>
-            <h1 className='font-playfairDisplay text-6xl italic text-white font-medium pl-5 my-4'>Hello</h1>
+        <div className='bg-folderYellow p-3 rounded-r-md'>
+            <h1 className='font-playfairDisplay text-7xl italic text-white font-medium pl-5 my-4'>Hello</h1>
             <h1 className='font-roboto text-6xl italic text-white font-bold text-right pr-7'>World</h1>
             <span className="flex justify-end mt-7">
                 <SecondaryButton>Get to know me</SecondaryButton>
