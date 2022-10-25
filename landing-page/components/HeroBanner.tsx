96b9a5c4e608e7d78d53276b5d822c78d3d1f8e8
Will Joseph <programergeek@gmail.com>
2022-10-25 05:59:55 +0000

    add missing screen size specifications

--- landing-page/components/HeroBanner.tsx
+++ landing-page/components/HeroBanner.tsx
@@ -4,8 +4,8 @@ import { SecondaryButton } from './Buttons'
 export const HeroBanner: React.FC = () => {
     return (
         <div className='bg-folderYellow p-3 rounded-r-md'>
-            <h1 className='font-playfairDisplay text-7xl sm:text-9xl italic text-white font-medium pl-12 my-7'>Hello</h1>
-            <h1 className='font-roboto text-6xl sm:text-9xl italic text-white font-bold text-right pr-20'>World</h1>
+            <h1 className='font-playfairDisplay text-7xl sm:text-9xl italic text-white font-medium pl-5 sm:pl-12 my-4 sm:my-7'>Hello</h1>
+            <h1 className='font-roboto text-6xl sm:text-9xl italic text-white font-bold text-right pr-7 sm:pr-20'>World</h1>
             <span className="flex justify-end mt-7 sm:mt-14">
                 <SecondaryButton>Get to know me</SecondaryButton>
             </span>
