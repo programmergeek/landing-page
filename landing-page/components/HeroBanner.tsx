f9423c974605ec865bc380353ecb3b2f0d275d10
Will Joseph <programergeek@gmail.com>
2022-10-25 05:56:12 +0000

    increase text size and white space for tablets

--- landing-page/components/HeroBanner.tsx
+++ landing-page/components/HeroBanner.tsx
@@ -4,9 +4,9 @@ import { SecondaryButton } from './Buttons'
 export const HeroBanner: React.FC = () => {
     return (
         <div className='bg-folderYellow p-3 rounded-r-md'>
-            <h1 className='font-playfairDisplay text-7xl italic text-white font-medium pl-5 my-4'>Hello</h1>
-            <h1 className='font-roboto text-6xl italic text-white font-bold text-right pr-7'>World</h1>
-            <span className="flex justify-end mt-7">
+            <h1 className='font-playfairDisplay text-7xl sm:text-9xl italic text-white font-medium pl-12 my-7'>Hello</h1>
+            <h1 className='font-roboto text-6xl sm:text-9xl italic text-white font-bold text-right pr-20'>World</h1>
+            <span className="flex justify-end mt-7 sm:mt-14">
                 <SecondaryButton>Get to know me</SecondaryButton>
             </span>
         </div>
