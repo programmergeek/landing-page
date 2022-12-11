1a23e9e39bff259863e5c0f4925edc08526cac67
Will <programergeek@gmail.com>
2022-12-11 16:19:50 +0200

    create classes to control project row behaviour

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,35 +1,59 @@
 import React from 'react'
-import { ProjectCard } from '../Cards'
+import { ProjectCard } from '../Cards/Cards'
 import { Header } from '../Header'
+import styles from './Projects.module.css'
 
 export const Projects: React.FC = () => {
     return(
         <section className="mt-3" id="Projects">
           <Header>Projects</Header>
-          <div className="flex flex-col lg:flex-row justify-center gap-10 lg:gap-14 lg:flex-wrap">
-            <div className="w-fit mr-auto 2xl:m-0">
+          <div className="flex flex-col gap-6">
+            <div className={styles['project-row']}>
               <ProjectCard
-                projectName="The Blog"
+                title="The Blog"
                 description="Take a peek into my what I've been learning."
-                demoLink="https://blog.willjoseph.xyz"
-                repoLink="https://github.com/programmergeek/the-blog"
+                links={{
+                  repo: "https://github.com/programmergeek/the-blog",
+                  project: "https://blog.willjoseph.xyz"
+                }}
+                size="full"
+                tags={['React', 'RestAPI', 'Contentful', 'Tailwind CSS']}
+                thumbnail="https://picsum.photos/1200/500"
+                />
+                <ProjectCard
+                title="The Blog"
+                description="Take a peek into my what I've been learning."
+                links={{
+                  repo: "https://github.com/programmergeek/the-blog",
+                  project: "https://blog.willjoseph.xyz"
+                }}
+                tags={['React', 'RestAPI', 'Contentful', 'Tailwind CSS']}
+                thumbnail="https://picsum.photos/700/500"
+                size='full'
                 />
             </div>
-            <div className="w-fit ml-auto 2xl:m-0">
+            <div className={styles['project-row-reverse']}>
               <ProjectCard
-                reverse
-                projectName="The Blog"
+                title="The Blog"
                 description="Take a peek into my what I've been learning."
-                demoLink="https://blog.willjoseph.xyz"
-                repoLink="https://github.com/programmergeek/the-blog"
+                links={{
+                  repo: "https://github.com/programmergeek/the-blog",
+                  project: "https://blog.willjoseph.xyz"
+                }}
+                tags={['React', 'RestAPI', 'Contentful', 'Tailwind CSS']}
+                thumbnail="https://picsum.photos/700/500"
+                size='full'
                 />
-            </div>
-            <div className="w-fit mr-auto 2xl:m-0">
               <ProjectCard
-                projectName="The Blog"
+                title="The Blog"
                 description="Take a peek into my what I've been learning."
-                demoLink="https://blog.willjoseph.xyz"
-                repoLink="https://github.com/programmergeek/the-blog"
+                links={{
+                  repo: "https://github.com/programmergeek/the-blog",
+                  project: "https://blog.willjoseph.xyz"
+                }}
+                size="full"
+                tags={['React', 'RestAPI', 'Contentful', 'Tailwind CSS']}
+                thumbnail="https://picsum.photos/1200/500"
                 />
             </div>
           </div>
