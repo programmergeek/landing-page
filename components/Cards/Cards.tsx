c145c071a76899c64174e2ea83555b357f9358e8
Will <programergeek@gmail.com>
2022-12-12 14:39:14 +0200

    make thumbnail image a background image for the card whose edges overflow and are hidden as the screen size becomes smaller

--- components/Cards/Cards.tsx
+++ components/Cards/Cards.tsx
@@ -34,12 +34,12 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ ...props }) => {
   const [mouseHover, updateMouseHover] = useState<boolean>(false)
 
     return (
-      <div className={`${styles[`card-${props.size? props.size: "md"}`]} rounded-md relative`} onMouseEnter={() => updateMouseHover(true)} onMouseLeave={() => updateMouseHover(false)}>
-        <img src={props.thumbnail} className="rounded-xl h-[500px]" alt="" />
+      <div className={`${styles[`card-${props.size? props.size: "md"}`]} rounded-md relative overflow-hidden`} onMouseEnter={() => updateMouseHover(true)} onMouseLeave={() => updateMouseHover(false)}>
+        <div className="overflow-hidden w-12/12 h-[500px]" style={{backgroundImage: `url(${props.thumbnail})`, backgroundPosition: "center center", backgroundRepeat: "no-repeat"}}></div>
         <div className={`w-full h-full rounded-xl bg-black absolute top-0 opacity-50 ${mouseHover? "": "hidden"}`}></div>
         <div className={`absolute top-0 h-full w-full ${mouseHover? "": "hidden"}`}>
           <div className="w-fit h-fit mt-5 lg:mt-36 mx-auto">
-            <header className="text-3xl text-white text-center font-roboto"> {props.title} </header>
+            <h1 className="text-3xl text-white text-center font-roboto"> {props.title} </h1>
             <p className="text-white text-center font-roboto my-5"> {props.description} </p>
             <div className="max-w-[500px] flex flex-wrap gap-3 justify-center mb-5">
               {
