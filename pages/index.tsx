b672dc31bff4755be530c2868a0d6df389a33e93
Will <programergeek@gmail.com>
2022-12-13 19:36:52 +0200

    create down arrow pointing to the projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from 'next'
 import { ContactMe, HeroBanner, Navbar, Projects, About} from '../components'
-import styles from '../styles/Home.module.css'
+import { HiChevronDoubleDown } from 'react-icons/hi'
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Navbar />
-      <HeroBanner />
-      <div className={styles['sub-container']}>
-        <About />
-        <Projects />
+      <About />
+      <div className='w-full grid mt-16 sm:m-0'>
+        <HiChevronDoubleDown color='rgb(74 222 128)'  className='animate-bounce w-12 h-12 place-self-center'/>
       </div>
-        <ContactMe />
-        <div className='bg-green-400 text-center font-sourceCodePro py-3'>Made by a local internet nerd 💻</div>
+      <Projects />
+      <ContactMe />
+      <div className='bg-green-400 text-center font-sourceCodePro py-3'>Made by a local internet nerd 💻</div>
     </div>
   );
 }
