ee470d6bae5caa072dfbcdac6338b00b7c740187
Will <programergeek@gmail.com>
2022-12-13 03:19:40 +0200

    rework about me section

--- components/AboutMe/About.tsx
+++ components/AboutMe/About.tsx
@@ -1,53 +1,22 @@
 import React from 'react'
-import {Tags, Accent, Card} from '..'
+import {Tags, Accent, Card, Header} from '..'
 import styles from './AboutMe.module.css'
 
 export const About: React.FC = () => {
     return (
-      <div>
-        <Card className="relative">
-          <Accent className="absolute bottom-3 right-3 lg:bottom-6 lg:right-6 transform md:scale-150 lg:scale-[2.5]" />
-          <Accent className="absolute top-3 left-3 lg:top-6 lg:left-6 -rotate-180 transform md:scale-150 lg:scale-[2.5]" />
-          <div id={styles.content} className="w-full sm:w-10/12 md:w-9/12 mx-auto lg:w-7/12 2xl:w-6/12">
-            <h1 id={styles.aboutMe} className="font-sourceCodePro italic text-2xl lg:text-3xl font-semibold text-center lg:mb-11 mb-5">
-              About me
-            </h1>
-            <div id={styles.avatar}>
-                <img
-                  src="/images/avatar.png"
-                  alt="avatar"
-                  className="w-40 md:w-64 rounded-full"
-                />
-              </div>
-              <div id={styles.skills}>
-                <h3 className="font-sourceCodePro font-semibold italic md:text-xl">
-                  Skills
-                </h3>
-                <div className="flex flex-wrap gap-1 h-fit align-middle">
-                  {[
-                    "Git",
-                    "CSS",
-                    "HTML",
-                    "React",
-                    "Next.JS",
-                    "TypeScript",
-                    "JavaScript",
-                    "Tailwindcss",
-                  ].map((value) => (
-                    <Tags key={value} className="mr-0.5 mt-0.5">
-                      {value}
-                    </Tags>
-                  ))}
-                </div>
-              </div>
-            <p id={styles.content_aboutme} className="mx-auto mt-5 sm:m-0 font-roboto italic font-semibold text-xs lg:text-sm md:w-10/12">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
-              animi nemo optio maxime repellat exercitationem temporibus et
-              saepe vel iste necessitatibus neque est, quaerat veritatis
-              perferendis aliquam nesciunt officiis dolor?
-            </p>
+      <section className='mt-20 mb-24 md:mb-52'>
+          <Header>About Me</Header>
+          <div id={styles.content}>
+            <div className='place-self-center md:place-self-end'>
+              <img src="/images/avatar.png" className='rounded-full w-80' alt="" />
             </div>
-        </Card>
-      </div>
+            <p className='text-center place-self-center md:px-5'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Optio doloremque laboriosam consequuntur voluptatem pariatur sapiente alias doloribus 
+              culpa labore fugit itaque tempore, exercitationem fuga adipisci aliquid nam? 
+              Quisquam, deserunt sed!
+            </p>
+          </div>
+      </section>
     );
 }
