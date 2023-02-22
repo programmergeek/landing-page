c091a48b44595219354e69f358c737530811a2b4
Will <programergeek@gmail.com>
2023-02-22 08:47:43 +0200

    style: remodel header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-export const Header: React.FC<Props> = ({...props}) => {
-    return (
-        <div className='w-fit mx-auto relative'>
-            <h1 className='z-20 font-roboto text-3xl font-semibold text-center lg:mb-11 mb-5 text-green-700'>{props.children}</h1>
-            <div className='bg-green-500 w-[103%] h-3 absolute top-5 z-0 opacity-[30%]'></div>
-        </div>
-    )
-}
+export const Header: React.FC<Props> = ({ ...props }) => {
+  return (
+    <h1 className="font-bold text-2xl lg:text-4xl mb-2 md:mb-5">
+      {props.children}
+    </h1>
+  );
+};
