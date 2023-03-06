52cbdf7fc470422362da1a7e96dffcda8cbe22b9
Will <programergeek@gmail.com>
2023-03-06 22:22:53 +0200

    feat: give about me section an id for navigation

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -24,22 +24,26 @@ export const AboutMe = () => {
   ];
 
   return (
-    <section className="min-h-screen pt-5 md:pt-10 lg:pt-20 xl:pt-40">
-      <div className="flex flex-col gap-10">
-        <div>
-          <h1 className="font-bold text-2xl lg:text-4xl mb-2 md:mb-5">
-            About me...
-          </h1>
-          <p className="lg:text-xl">
-            {`
+    <>
+      <section
+        id="aboutMe"
+        className="min-h-screen pt-5 md:pt-10 lg:pt-20 xl:pt-40"
+      >
+        <div className="flex flex-col gap-10">
+          <div>
+            <h1 className="font-bold text-2xl lg:text-4xl mb-2 md:mb-5">
+              About me...
+            </h1>
+            <p className="lg:text-xl">
+              {`
             My first introduction to coding came in the ninth grade. 
             It's a funny story that was fueled by pettiness and some friendly 
             rivalry between a friend and a teacher. I just happened to be 
             caught in the middle of it.
             `}
-            <br />
-            <br />
-            {`
+              <br />
+              <br />
+              {`
             To make a long story short, said friend and teacher argued whether
              or not it was possible to teach an entire year's coursework within a 
              single school term (roughly two months). My friend argued yes while 
@@ -48,10 +52,10 @@ export const AboutMe = () => {
              I could barely remember what I had learned at the beginning of it. 
              That was the first time I had ever seen and written code and tried 
              to solve problems with it. It was fun.
-            `}
-            <br />
-            <br />
-            {`
+             `}
+              <br />
+              <br />
+              {`
             Fast forward a couple of years and I haven't stopped coding. I've experimented 
             with different parts of software engineering, dipped my toes into game development 
             and learned a bit of C# and C++ while experimenting with Unity and Unreal Engine. 
@@ -63,20 +67,24 @@ export const AboutMe = () => {
             through the power of the internet and I hope that you will allow me to do that 
             as part of your team.
             `}
-          </p>
-        </div>
-        <div>
-          <h1 className="font-bold text-2xl lg:text-4xl">...And my skills</h1>
-          <div className="grid grid-cols-4 md:flex flex-wrap gap-6 md:gap-10 lg:gap-20 mt-5 md:mt-10">
-            {skills.map((skill) => (
-              <div key={skill.label} className="flex flex-col justify-center">
-                <div className="w-full flex justify-center">{skill.icon}</div>
-                <span className="lg:text-xl text-center"> {skill.label} </span>
-              </div>
-            ))}
+            </p>
+          </div>
+          <div>
+            <h1 className="font-bold text-2xl lg:text-4xl">...And my skills</h1>
+            <div className="grid grid-cols-4 md:flex flex-wrap gap-6 md:gap-10 lg:gap-20 mt-5 md:mt-10">
+              {skills.map((skill) => (
+                <div key={skill.label} className="flex flex-col justify-center">
+                  <div className="w-full flex justify-center">{skill.icon}</div>
+                  <span className="lg:text-xl text-center">
+                    {" "}
+                    {skill.label}{" "}
+                  </span>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
